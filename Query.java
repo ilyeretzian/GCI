daf07b8a0326d6e6257758f45d485e1dfe16eb86
Joseph Robledo <144262300+JosephRobledo99@users.noreply.github.com>
2024-11-04 13:57:36 -0800

    Update Query.java

--- Query.java
+++ Query.java
@@ -1,1 +1,32 @@
+//This class is responsible for creating query objects to be stored in a list which is then returned to the professor
+public class Query {
 
+    //member variable
+    private String m_StudyArea;
+    private String m_SubjectInArea;
+    private String m_Question;
+
+    
+
+
+    //constructor of default ace of spades card.
+    public Query(String StudyArea, String SubjectInArea, String Question){
+        m_StudyArea=StudyArea;
+        m_SubjectInArea=SubjectInArea;
+        m_Question=Question;
+    }
+
+
+    public String getStudyArea(){
+        return m_StudyArea;
+    }
+
+    public String getSubjectInArea(){
+        return m_SubjectInArea;
+    }
+
+    public String getQuestion(){
+        return m_Question;
+    }
+
+}
